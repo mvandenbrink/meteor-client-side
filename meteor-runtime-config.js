775ff7855d2e2f41850221d84fb358d340cdade8
Marcel van den Brink <mvandenbrink@users.noreply.github.com>
2016-04-25 15:28:17 +0200

    Fixes overwriting config issue

--- meteor-runtime-config.js
+++ meteor-runtime-config.js
@@ -2,8 +2,12 @@
   var _ = Package['underscore']._;
   var config = this.__meteor_runtime_config__ || {};
 
-  __meteor_runtime_config__ = _.defaults({}, config, {
-    meteorEnv: {},
-    DDP_DEFAULT_CONNECTION_URL: 'http://localhost:3000'
-  });
+  __meteor_runtime_config__ = _.defaults(
+    {}, 
+    {
+      meteorEnv: {},
+      DDP_DEFAULT_CONNECTION_URL: 'http://localhost:3000'
+    },
+    config
+  );
 }).call(this);
